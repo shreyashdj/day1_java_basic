73405129a9abd9936260c1fa20b2340c80f98000
shreyashdj <shreyashjadhav1799@gmail.com>
2022-07-01 19:07:24 +0530

    Added: String compare

---
+++ src/String_compare.java
@@ -0,0 +1,21 @@
+
+public class String_compare {
+
+	public static void main(String[] args) {
+		
+		String first = "java basic";
+		String second = "java basic";
+		
+		if(first == second) {
+			System.out.println("True");
+		}
+		else {
+			System.out.println("False");
+		} 
+			
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
