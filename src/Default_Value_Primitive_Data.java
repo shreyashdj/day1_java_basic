d69ddbdba1902a9a9e91be8244cd9afdad52a2d8
shreyashdj <shreyashjadhav1799@gmail.com>
2022-07-01 19:01:53 +0530

    Added: Default values of primitive data types

---
+++ src/Default_Value_Primitive_Data.java
@@ -0,0 +1,29 @@
+
+public class Default_Value_Primitive_Data {
+	
+	static	int val1;
+	static	double val2;
+	static	boolean val3;
+	static	byte val4;
+	static	short val5;
+	static	long val6;
+	static	float val7;
+	static	char val8;
+
+
+	public static void main(String[] args) {
+		
+		System.out.println("int " +val1);
+		System.out.println("double " +val2);
+		System.out.println("boolean " +val3);
+		System.out.println("byte " +val4);
+		System.out.println("short " +val5);
+		System.out.println("long " +val6);
+		System.out.println("float " +val7);
+		System.out.println("char " +val8);	
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
