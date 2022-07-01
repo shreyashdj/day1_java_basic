93b327064f11d698cbb78baeb4bcb556e4f801f8
shreyashdj <shreyashjadhav1799@gmail.com>
2022-07-01 19:18:15 +0530

    Added: get name

---
+++ src/Get_name.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Get_name {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter a Name");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		
+		System.out.println("Name is " +name);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
