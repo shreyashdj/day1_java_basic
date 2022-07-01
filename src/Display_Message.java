27b9895ef8f0fd12aafa6ce7c64dbb5a0d438c8a
shreyashdj <shreyashjadhav1799@gmail.com>
2022-07-01 18:21:32 +0530

    Added: Display message program

---
+++ src/Display_Message.java
@@ -0,0 +1,12 @@
+
+public class Display_Message {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Hello \n"
+						+ "I am Shreyash \n"
+						+ "Welcome to Java basic");	
+		
+		// TODO Auto-generated method stub			
+	}
+}
